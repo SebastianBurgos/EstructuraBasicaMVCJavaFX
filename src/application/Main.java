bebe5369ef53d11c27dec08bb5a3ad961d1d29a4
Sebastian Burgos Puentes <sebastian.burgosp@uqvirtual.edu.co>
2022-10-24 20:47:02 -0500

    Presentacion powerpoint MVC Sebastian Burgos Puentes

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("Aplicacion Prestamo Objetos");
+		this.primaryStage.setTitle("Aplicacion");
 		mostrarVentanaMain();
 	}
 
